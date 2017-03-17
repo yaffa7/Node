ce9ed953d5ed16f98a610755bd2cfdd7fd45a1f4
yaffa7 <bmcmahon2@mail.csuchico.edu>
2017-03-16 23:59:59 -0700

    con

--- express-endpoint-demmo.js
+++ express-endpoint-demmo.js
@@ -3,16 +3,17 @@ var app = express();
 var fs = require('fs');
 
 app.use('/message', function(req,res) {
-  console.log('user requested endpoint');
+    console.log('user requested endpoint');
 
-  res.send('hello');
+    res.send('hello');
 });
 
 app.use('/users',function(req,res){
-  fs.readFile('./data1.json','utf-8',function(err,data) {
-    data = JSON.parse(data);
-    res.send(data);
-  });
+    console.log('Connected!');
+    fs.readFile('./data2.json','utf-8',function(err,data) {
+        data = JSON.parse(data);
+        res.send(data);
+    });
 });
 
 app.listen(4000);
